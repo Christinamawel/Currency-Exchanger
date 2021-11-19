512b420c3767f0a7aab2d48fa3abc711fdacc4d5
Christinamawel <christinamawel@gmail.com>
2021-11-19 15:25:16 -0800

    study how to use session storage to store API call and impliment in project

--- src/index.js
+++ src/index.js
@@ -6,27 +6,32 @@ import { Currency } from './currency.js';
 import { currencyConverter } from './currency.js';
 
 $(document).ready(function() {
+  Currency.getConverter()
+    .then(function (response) {
+      if(!response.conversion_rates) {
+        $("#showError").text(`${response}`);
+        $("#error").show();
+      } else {
+        sessionStorage.setItem('currencyConversions', JSON.stringify(response.conversion_rates));
+      }
+    });
+
   $('form#currency-exchanger').submit(function(event) {
     event.preventDefault();
+    const currencyExchangeRates = JSON.parse(sessionStorage.getItem('currencyConversions'));
     let currency = $("#currency").val();
     let USD = $("#USD").val();
     $("#USD").val('');
     $("#output-div").hide();
     $("#error").hide();
 
-    Currency.getConverter()
-      .then(function(response) {
-        if(!response.conversion_rates) {
-          $("#showError").text(`${response}`);
-          $("#error").show();
-        } else if (!response.conversion_rates[currency]) {
-          $("#showError").text(`cannot find conversion rate for ${currency}`);
-          $("#error").show();
-        } else {
-          let convertedAmount = currencyConverter(USD, response.conversion_rates[currency]);
-          $("#output").text(`${convertedAmount} ${currency}`);
-          $("#output-div").fadeIn();
-        }
-      });
+    if (!currencyExchangeRates[currency]) {
+      $("#showError").text(`cannot find conversion rate for ${currency}`);
+      $("#error").show();
+    } else {
+      let convertedAmount = currencyConverter(USD, currencyExchangeRates[currency]);
+      $("#output").text(`${convertedAmount} ${currency}`);
+      $("#output-div").fadeIn();
+    }
   });
 });
