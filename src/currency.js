1edce9e6251a71cc847733a3993faeba8a13866f
Christinamawel <christinamawel@gmail.com>
2021-11-19 09:19:07 -0800

    create basic UI logic in index.html

--- src/currency.js
+++ src/currency.js
@@ -1,14 +1,18 @@
-export default class Currency {
+export class Currency {
   static getConverter() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API-KEY}/latest/USD`)
-    .then (function(response) {
-      if(!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return error;
-    })
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then (function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
+}
+
+export function currencyConverter(USD, newCurrency) {
+  return Math.round((USD * newCurrency)*100)/100;
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-import Currency from './currency.js';
+import { Currency } from './currency.js';
+import { currencyConverter } from './currency.js';
 
+$(document).ready(function() {
+  $('form#currency-exchanger').submit(function(event) {
+    event.preventDefault();
+    let currency = $("#currency").val();
+    let USD = $("#USD").val();
+
+    Currency.getConverter()
+      .then(function(response) {
+        if(response.conversion_rates[currency]) {
+          let convertedAmount = currencyConverter(USD, response.conversion_rates[currency]);
+          $("#output").text(`${convertedAmount} ${currency}`)
+        } else if (response.conversion_rates.USD) {
+          $("#showError").text(`cannot find conversion rate for ${currency}`);
+        } else {
+          $("#showError").text(`Error: ${response.message}`)
+        }
+      });
+  });
+});
